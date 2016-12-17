e3afd2fd6be560f5b82dc1ab0d110accf74d0bbf
MarcosMesonero <100292865@alumnos.uc3m.es>
2016-12-17 20:22:29 +0100

    chat

--- src/main/java/es/uc3m/tiw/controladores/Controlador.java
+++ src/main/java/es/uc3m/tiw/controladores/Controlador.java
@@ -2,6 +2,7 @@ package es.uc3m.tiw.controladores;
 
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,18 +21,27 @@ public class Controlador {
 	@Autowired
 	private MensajeRepository mensajeDao;
 	
-	@RequestMapping(value = "/guardarMensaje", method = RequestMethod.POST)
-	public Mensaje guardarMensaje(@RequestParam Mensaje mensaje){
+	
+	@RequestMapping(value = "/guardarMensaje{ids}", method = RequestMethod.POST)
+	public Mensaje guardarMensaje(@RequestBody Mensaje mensaje, @PathVariable(value = "ids") Map <String,String> ids){
+	
+		String idEmisor = ids.get("idPropietario");
+		String idReceptor = ids.get("idReceptor");
+		//String mensajeDescripcion = mensaje.getMensaje();
+		
+		mensaje.setIdEmisor(idEmisor);
+		mensaje.setIdReceptor(idReceptor);
 		mensajeDao.save(mensaje);
+		
 		if (mensajeDao.findAll() == null) return null;
 		return mensaje;
 	}
 	
-   @RequestMapping(value="/bandejaEntrada/{idReceptor}", method=RequestMethod.GET)
+   /*@RequestMapping(value="/bandejaEntrada/{idReceptor}", method=RequestMethod.GET)
     public List<Mensaje> MensajesRecibidos(@PathVariable String emailReceptor){
     		List<Mensaje> MensajesRecibidos = mensajeDao.findByIdReceptor(emailReceptor);
     		if (MensajesRecibidos.isEmpty()) return null;
     		return MensajesRecibidos;
-    }
+    }*/
     
 }
